0c6c5491523bb908cc4be0466d5471c08b3f1c17
Sara-Ayash <sara05485@gmail.com>
2025-01-29 19:39:57 +0000

    init

---
+++ py1.q_3ex_2025dl.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python3
+import time
+import os 
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from clearml import Task, Logger
+from torchvision.models import resnet18, ResNet18_Weights
+
+
+SMOKING = 1
+NO_SMOKING = 0
+
+def get_label_by_filename(file_name: str):
+    return NO_SMOKING if file_name.startswith("notsmoking_") else SMOKING
+
+
+class SmokingDataset(datasets.VisionDataset):
+    def __init__(self, data_dir, include_transform=True):
+        self.data_dir = data_dir
+        self.transform = None if not include_transform else self.set_transform()
+        self.file_list = os.listdir(data_dir)
+        
+    def __len__(self):
+        return len(self.file_list)
+    
+    def __getitem__(self, idx):
+        file_name = self.file_list[idx]
+        file_path = os.path.join(self.data_dir, file_name)
+        
+        label = NO_SMOKING if file_name.startswith("notsmoking_") else SMOKING
+
+        image = Image.open(file_path).convert("RGB")
+        
+        # Apply transformations (if any)
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, label, file_name
+
+    def set_transform(self):
+        return transforms.Compose([
+            transforms.Resize((250, 250)),  # Resize images to 128x128
+            transforms.RandomRotation(20), 
+            transforms.RandomHorizontalFlip(0.5),
+            transforms.RandomVerticalFlip(0.3),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+class ResNet18ClassificationModel(nn.Module):
+    def __init__(self):
+        super(ResNet18ClassificationModel, self).__init__()
+        self.resnet = resnet18(weights=ResNet18_Weights.DEFAULT)
+        
+        # התאמת השכבה האחרונה לסיווג בינארי
+        num_features = self.resnet.fc.in_features  # מספר הפיצ'רים שיוצאים מהשכבה האחרונה
+        self.resnet.fc = nn.Sequential(
+            nn.Linear(num_features, 1),  # שכבה אחת עם נוירון אחד
+            nn.Sigmoid()  # סיגמואיד להסתברות בינארית
+        )
+
+    def forward(self, x):
+        return self.resnet(x)
+
+
+class BinaryClassificationModel(nn.Module):
+    def __init__(self):
+        super(BinaryClassificationModel, self).__init__()
+        self.conv = nn.Sequential(
+            # First convolutional block
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),  # Output: 250x250
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Down-sample: 125x125
+            
+            # Second convolutional block
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),  # Output: 125x125
+            nn.ReLU(),
+            # nn.MaxPool2d(kernel_size=2, stride=2),  # Down-sample: 63x63
+            
+            # Third convolutional block
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),  # Output: 63x63
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Down-sample: 32x32
+            
+            # Fourth convolutional block
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),  # Output: 32x32
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)  # Down-sample: 16x16
+        )
+        
+        # Fully connected layers
+        self.fully_connected = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128 * 15 * 15, 128),  # Adjust input size for 16x16 feature maps
+            nn.ReLU(),
+            nn.Linear(128, 1),  # Single output for binary classification
+            nn.Sigmoid()  # Sigmoid activation for probabilities
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.fully_connected(x)
+        return x
+
+
+if __name__ == '__main__':
+    task = Task.init(project_name='Ex4', task_name=f'Tranning Custom CNN {time.time()}')
+    # Prepare the tranning dataset
+    train_dataset: datasets.ImageFolder = SmokingDataset('student_318411840_vs/Training/Training/smoking')
+    validate_dataset: datasets.ImageFolder = SmokingDataset('student_318411840_vs/Validation/Validation/smoking')
+ 
+    train_loader = DataLoader(train_dataset, batch_size=20, shuffle=True, num_workers=4)
+    validation_loader = DataLoader(validate_dataset, batch_size=20, shuffle=True, num_workers=4)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    num_epochs = 4
+
+    model_path = "models_pth/model.pth"
+
+    print("Model file found. Loading the model...")
+    model = BinaryClassificationModel()
+    if os.path.exists(model_path):
+        model.load_state_dict(torch.load(model_path, weights_only=True))
+        
+ 
+    # Define the model, Loss Function and Optimizer
+    model: nn.Module = BinaryClassificationModel()
+    loss_function = nn.BCELoss() 
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+    # Train the Model
+    model.to(device) 
+
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+        
+        for images, labels, _ in train_loader:
+            images, labels = images.to(device), labels.to(device, dtype=torch.float32)
+
+            # Forward pass
+            outputs = model(images).squeeze()
+            loss = loss_function(outputs, labels)
+            
+            # Bpadding=1ackward pass and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            running_loss += loss.item()
+        
+        Logger.current_logger().report_scalar(
+            title="Loss VS Epoch", series="Loss", iteration=epoch, value=running_loss
+        )
+
+            
+        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(train_loader):.4f}")
+
+    torch.save(model.state_dict(), model_path)
+
+    # Validate the Model for binary classification
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels, file_names in validation_loader:
+            images, labels = images.to(device), labels.to(device, dtype=torch.float32)
+            outputs = model(images).squeeze()
+            predicted = (outputs > 0.5).float()
+
+            print("labels:\n", labels)
+            print("predicted:\n", predicted)
+            print("outputs:\n", outputs)
+            print("file_names:\n", file_names)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f'Validation Accuracy: {100 * correct / total:.2f}%')
